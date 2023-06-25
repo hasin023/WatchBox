656b2b088ea1201b90e68a06d4e18cd10bcf69f8
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-06-25 22:00:42 +0600

    Updated files

--- register.php
+++ register.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WatchBox</title>
+
+    <link rel="stylesheet" href="Assets/css/style.css">
 </head>
 <body>
     
+
+
+    <script src = "Assets/js/script.js"></script>
 </body>
 </html>
