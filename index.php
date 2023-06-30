72e3ff1fbcc94efaa4d01dfc3a1db8adad5c5961
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-01 03:02:56 +0600

    Added Category page container class

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once("includes/header.php");
 
 $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
+
+$containers = new CategoryContainer($con, $userLoggedIn);
+echo $containers->showAllCategories();
 ?>
 
 <!DOCTYPE html>
